6f9b137e064ed45535708dd0c833883a49e69394
Fabrizio Fasanando <fabriziofasanando2@gmail.com>
2021-05-29 22:04:30 +0200

    Ahora podemos iniciar y pausar el contador

--- build/www/js/index.js
+++ build/www/js/index.js
@@ -1,3 +1,6 @@
+let interval;
+let countdown_button = document.getElementById('countdown_button');
+let countdown_number = document.getElementById('countdown_number');
 let app = {
     // Application Constructor
     initialize: function () {
@@ -12,17 +15,21 @@ let app = {
     },
 
     receivedEvent: function (id) {
-        let countdown_button = document.getElementById('countdown_button');
-        countdown_button.addEventListener('click', this.startCountdown);
+        countdown_button.addEventListener('click', this.checkCountdown.bind(this));
     },
-
+    checkCountdown: function () {
+        if (countdown_button.classList.contains('started')) {
+            this.pauseCountdown();
+        } else {
+            this.startCountdown();
+        }
+    },
     startCountdown: function () {
-        let countdown_number = document.getElementById('countdown_number');
-
         // Iniciamos los valores del contador
-        let pomo_minutes = 25;
+        let pomo_minutes = 1;
         let pomo_seconds = 60;
-
+        countdown_button.classList.add('started');
+        countdown_button.innerHTML = 'Detener';
         // Restamos 1 a los minutos para que empiecen donde tocan
         pomo_minutes -= 1;
 
@@ -32,7 +39,7 @@ let app = {
             if (parseInt(pomo_seconds) === 0) {
                 // Si los minutos son 0 acabamos el contador
                 if (parseInt(pomo_minutes) === 0) {
-                    clearInterval(x);
+                    clearInterval(interval);
                     return;
                 }
                 pomo_minutes -= 1;
@@ -40,12 +47,18 @@ let app = {
             }
             // Esto se ejecuta si ninguna de las 2 situaciones anteriores sucede
             pomo_seconds -= 1;
-            pomo_minutes = pomo_minutes.toLocaleString(undefined, {minimumIntegerDigits:2, useGrouping:false});
-            pomo_seconds = pomo_seconds.toLocaleString(undefined, {minimumIntegerDigits:2, useGrouping:false});
+            pomo_minutes = pomo_minutes.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping: false});
+            pomo_seconds = pomo_seconds.toLocaleString(undefined, {minimumIntegerDigits: 2, useGrouping: false});
             countdown_number.innerHTML = `${pomo_minutes}:${pomo_seconds}`;
         }
 
-        let x = setInterval(counter, 1000);
+        interval = setInterval(counter, 1000);
+    },
+    pauseCountdown: function () {
+        clearInterval(interval);
+        countdown_button.innerHTML = 'Reanudar';
+        countdown_button.classList.remove('started');
+        countdown_button.classList.add('paused');
     }
 };
 
